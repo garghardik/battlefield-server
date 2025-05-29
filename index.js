aec0c0b93f229c73b06c419a556d453e5e320cfe
Hardik Garg <hardikgarg151@gmail.com>
2025-05-30 00:27:13 +0530

    updated index.js

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
 app.use(express.json());
 app.use("/battlefield",apiRoutes)
 
